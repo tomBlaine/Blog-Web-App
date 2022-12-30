1edaaa0d3efdcb31ba461ca526602cf56b30a6c3
tomBlaine <tom.blaine2002@gmail.com>
2022-12-30 19:14:49 +0000

    Added privileges field to user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->integer('privileges')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
